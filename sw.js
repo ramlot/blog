78f08cbb5476e4efdb49c6a215781e0a63109fe0
Inteon <42113979+inteon@users.noreply.github.com>
2020-06-05 12:10:54 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ var CACHE_NAME = 'blog-ramlot-eu-cache-v1';
 var urlsToCache = [
   '/',
   {%- for post in site.posts -%}
-  {{ post.url | escape }},
+  '{{ post.url | escape }}',
   {%- endfor -%}
 ];
 
